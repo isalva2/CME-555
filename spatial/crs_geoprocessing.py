967ce45f292dc5f1c2a572f327bc9733791d109e
Isaac Salvador <isalva2@uic.edu>
2023-10-05 20:02:20 -0500

    Working on geospatial

--- spatial/crs_geoprocessing.py
+++ spatial/crs_geoprocessing.py
@@ -19,34 +19,38 @@ crs3435_chicago_gdf.to_file(
         data_path, "interim/shp/chicago/chicago_epsg3435.shp")
     )
 
-# read and write cook county shape file
+# read and write cook county shape file https://data.cityofchicago.org/Facilities-Geographic-Boundaries/Boundaries-City/ewy2-6yfk
 file = "external/shp/Cook_County_Border/Cook_County_Border.shp"
 
-gpd.read_file(
+# read file
+cook = gpd.read_file(
     os.path.join(
         data_path, file
+        )
     )
-).to_crs(3435).to_file(
+
+# write to interim
+cook.to_crs(3435).to_file(
     os.path.join(
         data_path, "interim/shp/cook/cook_epsg3435.shp")
     )
 
-# filter counties in CMAP region and export to interim
+# filter counties in CMAP region and export to interim https://hub-cookcountyil.opendata.arcgis.com/search?categories=boundaries%2Cgeometric%2Ctax%20district%2Ccounty%2Cstate%2Cfederal%2Cmunicipal%2Cunincorporated%20cook
 file = "external/shp/IL_BNDY_County/IL_BNDY_County_Py.shp"
 
 # list of counties in CMAP
 CMAP_counties = ["COOK", "DUPAGE", "KANE", "KENDALL", "LAKE", "MCHENRY", "WILL"]
 
+#read file
 counties = gpd.read_file(
     os.path.join(
         data_path, file
         )
     )
 
-counties[
-    counties["COUNTY_NAM"].isin(CMAP_counties)
-].to_crs(3435).to_file(
+# filter to CMAP counties and write to interim
+counties[counties["COUNTY_NAM"].isin(CMAP_counties)].to_crs(3435).to_file(
     os.path.join(
         data_path, "interim/shp/cmap/cmap_epsg3534.shp"
+        )
     )
-)
